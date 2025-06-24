c8174b95617e57be92fcd90e1c3bca36a69c806b
Артемий <freedomskyleva@gmail.com>
2025-06-24 13:36:40 +0600

    feat: add palindrome realization

---
+++ practice2025/task01/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace task01;
+
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string input)
+    {
+        if (string.IsNullOrEmpty(input)) return false;
+        
+        string lower = input.ToLower();
+        StringBuilder sb = new StringBuilder();
+
+        foreach (var chr in lower)
+        {
+            if (!char.IsWhiteSpace(chr) && !char.IsPunctuation(chr))
+            {
+                sb.Append(chr);
+            }
+        }
+        
+        string final = sb.ToString();
+        string reverse = new string(final.Reverse().ToArray());
+
+        return reverse == final;
+    }
+}

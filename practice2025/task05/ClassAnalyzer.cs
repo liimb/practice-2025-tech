4d84bcc6876ef6d5aa0810fd717133bf881bd68e
Артемий <freedomskyleva@gmail.com>
2025-06-25 00:24:35 +0600

    feat: realization task05

---
+++ practice2025/task05/ClassAnalyzer.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+namespace task05;
+
+public class ClassAnalyzer(Type type)
+{
+    private Type _type = type;
+
+    public IEnumerable<string> GetPublicMethods() =>
+        _type.GetMethods().Select(m => m.Name).ToList();
+    
+
+    public IEnumerable<string> GetMethodParams(string methodName) =>
+         _type.GetMethod(methodName).GetParameters().Select(m => m.Name).ToList();
+    
+    public IEnumerable<string> GetAllFields() => _type.GetRuntimeFields().Select(m => m.Name).ToList();
+
+    public IEnumerable<string> GetProperties() => _type.GetProperties().Select(m => m.Name).ToList();
+
+    public bool HasAttribute<T>() where T : Attribute => _type.IsDefined(typeof(T));
+}

eb9c8efe0015274bdefad9c9dbd81312398636ea
Артемий <freedomskyleva@gmail.com>
2025-06-24 17:09:41 +0600

    feat: iterators task

---
+++ practice2025/task03tests/IteratorTests.cs
@@ -0,0 +1,41 @@
+using task03;
+
+namespace task03tests;
+
+public class IteratorTests
+{
+    [Fact]
+    public void CustomCollection_GetEnumerator_ReturnsAllItems()
+    {
+        var collection = new CustomCollection<int> { 1, 2 };
+
+        var result = collection.ToList();
+
+        Assert.Equal(new[] { 1, 2 }, result);
+    }
+
+    [Fact]
+    public void GetReverseEnumerator_ReturnsItemsInReverseOrder()
+    {
+        var collection = new CustomCollection<int> { 1, 2 };
+
+        var result = collection.GetReverseEnumerator().ToList();
+        Assert.Equal([2, 1], result);
+    }
+
+    [Fact]
+    public void GenerateSequence_ReturnsCorrectSequence()
+    {
+        var sequence = CustomCollection<int>.GenerateSequence(5, 3).ToList();
+        Assert.Equal([5, 6, 7], sequence);
+    }
+    
+    [Fact]
+    public void FilterAndSort_ReturnsFilteredAndSortedItems()
+    {
+        var collection = new CustomCollection<int> { 3, 1, 2 };
+    
+        var result = collection.FilterAndSort(x => x > 1, x => x).ToList();
+        Assert.Equal([2, 3], result);
+    }
+}

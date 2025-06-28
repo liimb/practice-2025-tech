ba1b0a21f483407c44829914c984377474e6e915
Артемий <freedomskyleva@gmail.com>
2025-06-28 20:44:25 +0600

    fix: HasAttribute method

--- practice2025/task05/ClassAnalyzer.cs
+++ practice2025/task05/ClassAnalyzer.cs
@@ -9,7 +9,6 @@ public class ClassAnalyzer(Type type)
     public IEnumerable<string> GetPublicMethods() =>
         _type.GetMethods().Select(m => m.Name).ToList();
     
-
     public IEnumerable<string> GetMethodParams(string methodName) =>
          _type.GetMethod(methodName).GetParameters().Select(m => m.Name).ToList();
     
@@ -17,5 +16,5 @@ public class ClassAnalyzer(Type type)
 
     public IEnumerable<string> GetProperties() => _type.GetProperties().Select(m => m.Name).ToList();
 
-    public bool HasAttribute<T>() where T : Attribute => _type.IsDefined(typeof(T));
+    public bool HasAttribute<T>() where T : Attribute => _type.IsDefined(typeof(T), inherit: true);
 }

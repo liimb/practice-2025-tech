edbae87275c5ca800d16ac69c06399e8fb6f13b7
Артемий <freedomskyleva@gmail.com>
2025-07-03 22:56:08 +0600

    test: update tests

--- practice2025/task08tests/FileSystemCommandsTests.cs
+++ practice2025/task08tests/FileSystemCommandsTests.cs
@@ -7,13 +7,21 @@ public class FileSystemCommandsTests
     [Fact]
     public void DirectorySizeCommand_ShouldCalculateSize()
     {
-        var testDir = Path.Combine(Path.GetTempPath(), "TestDir");
+        var testDir = Path.Combine(Path.GetTempPath(), "dir");
         Directory.CreateDirectory(testDir);
-        File.WriteAllText(Path.Combine(testDir, "test1.txt"), "Hello");
-        File.WriteAllText(Path.Combine(testDir, "test2.txt"), "World");
+        
+        var file1 = Path.Combine(testDir, "file1.txt");
+        var file2 = Path.Combine(testDir, "file2.txt");
+        
+        File.WriteAllText(file1, "Hello");
+        File.WriteAllText(file2, "World!");
 
+        var expectedSize = new FileInfo(file1).Length + new FileInfo(file2).Length;
+        
         var command = new DirectorySizeCommand(testDir);
-        command.Execute(); // Проверяем, что не возникает исключений
+        command.Execute();
+        
+        Assert.Equal(expectedSize, command.Size);
 
         Directory.Delete(testDir, true);
     }
@@ -21,13 +29,20 @@ public class FileSystemCommandsTests
     [Fact]
     public void FindFilesCommand_ShouldFindMatchingFiles()
     {
-        var testDir = Path.Combine(Path.GetTempPath(), "TestDir");
+        var testDir = Path.Combine(Path.GetTempPath(), "TestDir2");
         Directory.CreateDirectory(testDir);
-        File.WriteAllText(Path.Combine(testDir, "file1.txt"), "Text");
-        File.WriteAllText(Path.Combine(testDir, "file2.log"), "Log");
+        
+        var txtFile = Path.Combine(testDir, "file1.txt");
+        var logFile = Path.Combine(testDir, "file2.log");
+
+        File.WriteAllText(txtFile, "Text");
+        File.WriteAllText(logFile, "Log");
 
         var command = new FindFilesCommand(testDir, "*.txt");
-        command.Execute(); // Должен найти 1 файл
+        command.Execute();
+        
+        Assert.Single(command.FoundFiles);
+        Assert.Contains(txtFile, command.FoundFiles);
 
         Directory.Delete(testDir, true);
     }

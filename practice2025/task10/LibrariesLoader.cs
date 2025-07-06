865b9e3c8260671c72a5180fc1175df402b32773
Артемий <freedomskyleva@gmail.com>
2025-07-06 16:14:42 +0600

    test: add LibrariesLoader test

--- practice2025/task10/LibrariesLoader.cs
+++ practice2025/task10/LibrariesLoader.cs
@@ -2,8 +2,15 @@ namespace task10;
 
 public static class LibrariesLoader
 {
-    public static void Load(string path)
+    public static void Load(string pathToDirectory)
     {
+        var dllPaths = Directory.GetFiles(pathToDirectory, "*.dll");
+        var dllCount = dllPaths.Length;
+        var graph = new int[dllCount, dllCount];
         
+        dllPaths.ToList().ForEach(d =>
+        {
+            
+        });
     }
 }
--- practice2025/task10tests/LibrariesLoaderTests.cs
+++ practice2025/task10tests/LibrariesLoaderTests.cs
@@ -7,7 +7,14 @@ public class LibrariesLoaderTests
     [Fact]
     public void LibrariesLoader_HasCorrectConsolePrint()
     {
-        string expected = "";
-        LibrariesLoader.Load("");
+        var output = new StringWriter();
+        Console.SetOut(output);
+        
+        string expected = "Первый плагин\nВторой плагин\nТретий плагин\n\n";
+        
+        string path = Path.Combine(AppContext.BaseDirectory, "Plugins");
+        LibrariesLoader.Load(path);
+        
+        //Assert.Equal(expected, output.ToString());
     }
 }

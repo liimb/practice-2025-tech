19bc9130eb95463870135ca7c63244be00c11ea3
Артемий <freedomskyleva@gmail.com>
2025-07-10 15:11:45 +0600

    feat: CalculatorGenerator realization

--- practice2025/task11/CalculatorGenerator.cs
+++ practice2025/task11/CalculatorGenerator.cs
@@ -1,11 +1,17 @@
-﻿namespace task11;
+﻿using System.Runtime.Loader;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace task11;
 
 public static class CalculatorGenerator
 {
-    public static void GenerateCalculator()
+    public static ICalculator? GenerateCalculator()
     {
-        string calculator = """
-                            public class Calculator
+        string code = """
+                            using task11;
+                            
+                            public class Calculator : ICalculator
                                     {
                                         public int Add(int a, int b) => a + b;
                                         public int Minus(int a, int b) => a - b;
@@ -13,5 +19,35 @@ public static class CalculatorGenerator
                                         public int Div(int a, int b) => a / b;
                                     }
                             """;
+        
+        var syntaxTree = CSharpSyntaxTree.ParseText(code);
+
+        var references = AppDomain.CurrentDomain.GetAssemblies()
+            .Where(a => !a.IsDynamic && !string.IsNullOrWhiteSpace(a.Location))
+            .Select(a => MetadataReference.CreateFromFile(a.Location))
+            .ToList();
+        
+        var compilation = CSharpCompilation.Create(
+            assemblyName: "Calculator",
+            syntaxTrees: [syntaxTree],
+            references: references,
+            options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
+        );
+
+        using var ms = new MemoryStream();
+        var result = compilation.Emit(ms);
+
+        ms.Seek(0, SeekOrigin.Begin);
+        var assembly = AssemblyLoadContext.Default.LoadFromStream(ms);
+        var type = assembly.GetType("Calculator");
+        
+        if (type == null) return null;
+        
+        var instance = Activator.CreateInstance(type);
+
+        ICalculator calculator = (ICalculator)instance!;
+        
+        return calculator;
+
     }
 }
